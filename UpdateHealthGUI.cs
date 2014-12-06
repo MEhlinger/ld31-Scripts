f87e95a95629db2a11e98b272b0e54522255aef5
Marshall Ehlinger <mehlinger333@gmail.com>
2014-12-06 17:22:53 -0500

    Adds GUI text for player stats

---
+++ UpdateHealthGUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateHealthGUI : MonoBehaviour 
+{
+
+	public Text text;
+	public GameObject PC;
+	private float hpVal;
+
+
+	public void Update()
+	{
+		hpVal = PC.GetComponent<PlayerController>().health;
+		text.text = "BODY : " + hpVal.ToString();
+	}
+}
+
---
+++ UpdateSanityGUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateSanityGUI : MonoBehaviour 
+{
+
+	public Text text;
+	public GameObject PC;
+	private float sanVal;
+
+
+	public void Update()
+	{
+		sanVal = PC.GetComponent<PlayerController>().sanity;
+		text.text = "MIND : " + sanVal.ToString();
+	}
+}
