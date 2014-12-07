4c42f1594732b772a64114238022f9dc412064a0
Marshall Ehlinger <mehlinger333@gmail.com>
2014-12-06 22:13:43 -0500

    Added player attacks w/o animation

---
+++ PlayerAttack.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+	private float attackTimer;
+	private bool canAttack;
+	private Vector3 oldPos;
+
+	public bool attacking;
+
+	void Start() 
+	{
+		canAttack = true;
+		attackTimer = 70;
+	}
+
+
+	void Update()
+	{
+		attackTimer --;
+
+		if  (attackTimer <= 0)
+		{
+			attackTimer = 70;
+			canAttack = true;
+		}
+
+		if ((Input.GetKeyDown (KeyCode.LeftShift)) && (canAttack))
+		{
+			attacking = true;
+			oldPos = transform.position;
+			transform.position += new Vector3(-0.05f, 0.0f, 0.0f);
+			canAttack = false;
+		}
+		else if ((Input.GetKeyDown (KeyCode.RightShift)) && (canAttack))
+		{
+			attacking = true;
+			oldPos = transform.position;
+			transform.position += new Vector3(0.05f, 0.0f, 0.0f);
+			canAttack = false;
+		}
+	}
+
+
+	void LateUpdate()
+	{
+		if (attacking)
+		{
+			transform.position = oldPos;
+			attacking = false;
+		}
+	}
+}
