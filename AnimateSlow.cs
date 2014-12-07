5283760b31b364ae184737ed5d252c5162067440
Marshall Ehlinger <mehlinger333@gmail.com>
2014-12-07 13:22:59 -0500

    Adds simple 2-frame animation control script

---
+++ AnimateSlow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateSlow : MonoBehaviour {
+
+	public Sprite frame1, frame2;
+	public float aniSpeed; // in frames
+
+	private float aniSpeedTimer;
+	private SpriteRenderer spriteRenderer;
+
+	void Start()
+	{
+		aniSpeedTimer = aniSpeed;
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		aniSpeedTimer --;
+
+		if (aniSpeedTimer <= 0)
+		{
+
+			if (spriteRenderer.sprite == frame1)
+			{
+				spriteRenderer.sprite = frame2;
+			}
+
+			else
+			{
+				spriteRenderer.sprite = frame1;
+			}
+
+			aniSpeedTimer = aniSpeed;
+
+		}
+	}
+
+}
