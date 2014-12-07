5ff57b3dc129e2698bcd672183ee70196f30a0b2
Marshall Ehlinger <mehlinger333@gmail.com>
2014-12-06 20:38:35 -0500

    Adds food retrieval at fridge, and health+, sanity-

--- FoodEat.cs
+++ FoodEat.cs
@@ -6,6 +6,6 @@ public class FoodEat : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		rigidbody2D.AddForce(new Vector2(0.0f, 150.0f));
+		rigidbody2D.AddForce(new Vector2(0.0f, 200.0f));
 	}
 }
---
+++ GetFood.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetFood : MonoBehaviour 
+{
+
+	private bool atFridge;
+	private bool fridgeReady;
+	private float fridgeTimer;
+
+	public GameObject PC;
+
+	void Start()
+	{
+		fridgeTimer = 300;
+		fridgeReady = true;
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject == PC)
+		{
+			atFridge = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject == PC)
+		{
+			atFridge = false;
+		}
+	}
+
+	void Update()
+	{
+		fridgeTimer --;
+
+
+		if (fridgeTimer <= 0)
+		{
+			fridgeReady = true;
+			fridgeTimer = 300;
+		}
+
+
+		if (Input.GetKeyDown (KeyCode.Space) && atFridge && fridgeReady)
+		{
+			GameObject food = Instantiate(Resources.Load("Mushroom"), transform.position, transform.rotation) as GameObject;
+			fridgeReady = false;
+			PC.GetComponent<PlayerController>().sanity -= 2;
+			PC.GetComponent<PlayerController>().health += 10;
+			if (PC.GetComponent<PlayerController>().health > 100)
+			{
+				PC.GetComponent<PlayerController>().health = 100;
+			}
+		}
+	}
+}
+
