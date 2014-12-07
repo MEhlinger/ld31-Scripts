74e4c0c60e50e73914275e15b2a6ea938d71f9ff
Marshall Ehlinger <mehlinger333@gmail.com>
2014-12-06 19:47:52 -0500

    Adds food item movement and destroy when offscreen

---
+++ FoodEat.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodEat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigidbody2D.AddForce(new Vector2(0.0f, 150.0f));
+	}
+}
